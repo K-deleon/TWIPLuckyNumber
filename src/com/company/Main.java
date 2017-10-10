8a6dd2a67028c44af432ddd0534018065452b8c4
kd944 <deleon_k@ymail.com>
2017-10-10 08:36:05 -0400

    Added some nice messages :)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,7 +13,20 @@ public class Main {
 
         int num = (int)(Math.random()*10);
         int luckynum = (int)Math.pow(num, 2);
-        System.out.println("Hey " + name + "! Your lucky number is " + luckynum + ", " + num);
+        System.out.println("Hey " + name + "! Your lucky number is " + luckynum);
+
+        int randNum = (int)(Math.random()*5);
+        if (randNum == 0){
+            System.out.println("Have a nice day!");
+        } else if (randNum == 1){
+            System.out.println("Have a great day!");
+        } else if (randNum == 2){
+            System.out.println("Have an awesome day!");
+        } else if (randNum == 3){
+            System.out.println("Have a fantastic day!");
+        } else if (randNum == 4){
+            System.out.println("Have a super day!");
+        }
 
     }
 }
