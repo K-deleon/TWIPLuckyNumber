142c96acf214715e68c7efb31e31f9b3b38087a5
kd944 <deleon_k@ymail.com>
2017-10-10 08:24:18 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import  java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Scanner nameInput = new Scanner (System.in);
+        System.out.println("Hi! What's your name?");
+        String name = nameInput.nextLine();
+
+        int num = (int)(Math.random()*10);
+        int luckynum= (int)Math.pow(num, 2);
+        System.out.println("Hey " + name + "! Your lucky number is " + luckynum);
+
+    }
+}
